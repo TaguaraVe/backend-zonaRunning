bb8b2df629452b50654c1870a19fda88df498994
Youssef S <youssef.sabbagh.dev@gmail.com>
2023-08-11 01:59:41 -0400

    acotando

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import app from "./app"
-// import {connectDB} from './utils/connection'
+import { connectDB } from "./utils/connection"
 
 const PORT = process.env.PORT || 3000
 
 const main = () => {
-  // connectDB()
+  connectDB()
   app.listen(PORT)
   console.log(`Server running on port ${PORT}`)
 }
